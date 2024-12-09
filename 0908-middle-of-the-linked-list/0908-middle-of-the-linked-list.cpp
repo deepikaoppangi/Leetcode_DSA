26a821c69eb23ee5721bd7ff442c0cc89c3dbde9
deepikaoppangi <126076250+deepikaoppangi@users.noreply.github.com>
2024-12-10 00:41:21 +0530

    Time: 0 ms (100%), Space: 9.9 MB (13.41%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
@@ -8,24 +8,37 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* middleNode(ListNode* head) {
+//         int cnt = 0 ;
+//         ListNode* temp = head;
+//         while(temp!=NULL){
+//             cnt++;
+//             temp=temp->next;
+//         }
+//         int k =0;
+//         temp = head;
+//         while(temp!=NULL){
+//             k++;
+//             if(k==(cnt/2)+1){
+//                 return temp;
+//             }
+//             temp=temp->next;
+//         }
+//         return NULL;
+//     }
+// };
+
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        int cnt = 0 ;
-        ListNode* temp = head;
-        while(temp!=NULL){
-            cnt++;
-            temp=temp->next;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        int k =0;
-        temp = head;
-        while(temp!=NULL){
-            k++;
-            if(k==(cnt/2)+1){
-                return temp;
-            }
-            temp=temp->next;
-        }
-        return NULL;
+        return slow; 
     }
 };
