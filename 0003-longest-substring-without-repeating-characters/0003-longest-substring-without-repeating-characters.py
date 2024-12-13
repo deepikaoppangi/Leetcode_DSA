000ed0bbd64e5d83802049088a599d82e305158e
deepikaoppangi <126076250+deepikaoppangi@users.noreply.github.com>
2024-12-13 17:17:48 +0530

    Time: 15 ms (80.9%), Space: 17.3 MB (14.11%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,18 +3,17 @@ class Solution:
         char_map = {}
         start = 0  # Start of the sliding window
         max_len = 0  # To store the maximum length found
-        
-        for end in range(len(s)):
-            char = s[end]
+        for i in range(len(s)):
+            char = s[i]
             # If the character is in the window, move start to the right of its last position
             if char in char_map and char_map[char] >= start:
                 start = char_map[char] + 1
             
             # Update the last position of the character
-            char_map[char] = end
+            char_map[char] = i
             
             # Calculate the window size and update max_len if necessary
-            max_len = max(max_len, end - start + 1)
+            max_len = max(max_len, i - start + 1)
         
         return max_len
         
