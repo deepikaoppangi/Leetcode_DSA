53b3fa89049db6619597be4e54334cc511161184
deepikaoppangi <126076250+deepikaoppangi@users.noreply.github.com>
2024-12-21 17:35:08 +0530

    Time: 3 ms (98.14%), Space: 11.4 MB (30.83%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -6,17 +6,42 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode *detectCycle(ListNode *head) {
+//         map<ListNode* ,int>mpp;
+//         ListNode* temp=head;
+//         while(temp){
+//             if(mpp.find(temp)!=mpp.end()){
+//                 return temp;
+//             }
+//             mpp[temp]=1;
+//             temp=temp->next;
+//         }
+//         return NULL;
+//     }
+// };
+
+
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        map<ListNode* ,int>mpp;
-        ListNode* temp=head;
-        while(temp){
-            if(mpp.find(temp)!=mpp.end()){
-                return temp;
+        if(head==NULL){
+            return NULL;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow==fast){
+                ListNode* entry = head;
+                while(entry!=slow){
+                    slow=slow->next;
+                    entry = entry->next;
+                }
+                return entry;
             }
-            mpp[temp]=1;
-            temp=temp->next;
         }
         return NULL;
     }
