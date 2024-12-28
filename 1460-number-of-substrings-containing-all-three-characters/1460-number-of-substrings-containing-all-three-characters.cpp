102a2886382ed3d5c97d1e7f9c45ddc67d0e14a3
deepikaoppangi <126076250+deepikaoppangi@users.noreply.github.com>
2024-12-28 20:58:28 +0530

    Time: 0 ms (100%), Space: 11 MB (58.53%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -50,9 +50,7 @@ public:
         int hash[3]={-1,-1,-1};
         for(int i=0;i<n;i++){
             hash[s[i]-'a']=i;
-            if(hash[0]!=-1 && hash[1]!=-1 && hash[2]!=-1){
-                cnt = cnt + min(hash[0] , min(hash[1],hash[2]) ) + 1;
-            }
+            cnt = cnt + min(hash[0] , min(hash[1],hash[2]) ) + 1; 
         }
         return cnt;
     }
