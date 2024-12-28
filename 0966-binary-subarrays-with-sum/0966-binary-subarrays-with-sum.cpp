0a721f0641bc676cb3b70f91583f4119e5f99c5c
deepikaoppangi <126076250+deepikaoppangi@users.noreply.github.com>
2024-12-28 18:10:19 +0530

    Time: 0 ms (100%), Space: 32.7 MB (40.66%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,20 +1,49 @@
-//brute force
+// //brute force //accepted O(n^2)
+// class Solution {
+// public:
+//     int numSubarraysWithSum(vector<int>& nums, int goal) {
+//         int n = nums.size();
+//         int cnt=0;
+//         for(int i=0;i<n;i++){
+//             int sum=0;
+//             for(int j=i;j<n;j++){
+//                 sum=sum+nums[j];
+//                 if(sum==goal){
+//                     cnt++;
+//                 }
+//                 else if(sum>goal){
+//                     break;
+//                 }
+//             }
+//         }
+//         return cnt;
+//     }
+// };
+
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
+        if(goal<0){
+            return 0;
+        }
+        if(goal==0){
+            return atMost(nums, goal);
+        }
+        //no.of subarray with sum<=goal - no.of.suarray with sum<=goal-1
+        return atMost(nums, goal)-atMost(nums, goal - 1);
+    }
+    int atMost(vector<int>& nums, int goal) {
         int n = nums.size();
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            int sum=0;
-            for(int j=i;j<n;j++){
-                sum=sum+nums[j];
-                if(sum==goal){
-                    cnt++;
-                }
-                else if(sum>goal){
-                    break;
-                }
+        int cnt = 0;
+        int left = 0;
+        int sum = 0;
+        for (int right=0;right<n;right++) {
+            sum = sum+ nums[right]; 
+            while(sum>goal) {
+                sum = sum- nums[left];
+                left++;
             }
+            cnt = cnt + (right-left+1);
         }
         return cnt;
     }
