b75270fa49401805635a914dd166631a0f69f20f
deepikaoppangi <126076250+deepikaoppangi@users.noreply.github.com>
2024-12-28 20:52:38 +0530

    Time: 7 ms (65.81%), Space: 71.4 MB (65.13%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,28 +1,61 @@
+///my method //all passes
+
+// class Solution {
+// public:
+//     int numberOfSubarrays(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         int start=0;
+//         int cnt=0;
+//         int odd=0;
+//         vector<int>oddarr;
+//         for(int end=0;end<n;end++){
+//             if(nums[end]%2==1){
+//                 odd++;
+//                 oddarr.push_back(end);
+//             }
+//             while(odd>k){
+//                 if(nums[start]%2==1){
+//                     odd--;
+//                     oddarr.erase(oddarr.begin());
+//                 }
+//                 start++;
+//             }
+//             if(odd==k){
+//                 //cnt = cnt+ (end-start+1) - (end-begining);
+//                 cnt = cnt+ (oddarr[0]-start+1);
+//             }
+//         }
+//         return cnt;
+//     }
+// };
+
+
+
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& nums, int k) {
+    int fun(vector<int>& nums, int k){
         int n = nums.size();
-        int start=0;
         int cnt=0;
+        if(k<0){
+            return 0;
+        }
         int odd=0;
-        vector<int>oddarr;
+        int start=0;
         for(int end=0;end<n;end++){
             if(nums[end]%2==1){
                 odd++;
-                oddarr.push_back(end);
             }
             while(odd>k){
                 if(nums[start]%2==1){
                     odd--;
-                    oddarr.erase(oddarr.begin());
                 }
                 start++;
             }
-            if(odd==k){
-                //cnt = cnt+ (end-start+1) - (end-begining);
-                cnt = cnt+ (oddarr[0]-start+1);
-            }
+            cnt=cnt+(end-start+1);
         }
         return cnt;
+    }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return fun(nums,k)-fun(nums,k-1);
     }
 };
