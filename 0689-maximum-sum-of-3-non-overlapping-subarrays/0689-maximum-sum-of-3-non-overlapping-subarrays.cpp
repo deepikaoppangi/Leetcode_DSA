0722d12e9add0079e4a478398e38f0d6cd08ec01
deepikaoppangi <126076250+deepikaoppangi@users.noreply.github.com>
2024-12-29 20:12:05 +0530

    Time: 2 ms (65.77%), Space: 26.1 MB (69.54%) - LeetHub

--- 0689-maximum-sum-of-3-non-overlapping-subarrays/0689-maximum-sum-of-3-non-overlapping-subarrays.cpp
+++ 0689-maximum-sum-of-3-non-overlapping-subarrays/0689-maximum-sum-of-3-non-overlapping-subarrays.cpp
@@ -2,52 +2,48 @@ class Solution {
 public:
     vector<int> maxSumOfThreeSubarrays(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> prefixSums(n + 1, 0);
-        
-        // Compute prefix sums
-        for (int i = 0; i < n; ++i) {
-            prefixSums[i + 1] = prefixSums[i] + nums[i];
+        vector<int> windowSums(n - k + 1, 0);
+        vector<int> left(n - k + 1, 0), right(n - k + 1, 0);
+        vector<int> result(3, 0);
+
+        // Calculate sums of all k-length subarrays
+        int currentSum = 0;
+        for (int i = 0; i < n; i++) {
+            currentSum += nums[i];
+            if (i >= k) currentSum -= nums[i - k];
+            if (i >= k - 1) windowSums[i - k + 1] = currentSum;
         }
-        
-        vector<int> left(n, 0), right(n, n - k);
-        
-        // Compute best left indices
-        int maxSum = prefixSums[k] - prefixSums[0];
-        for (int i = k; i < n; ++i) {
-            if (prefixSums[i + 1] - prefixSums[i + 1 - k] > maxSum) {
-                left[i] = i + 1 - k;
-                maxSum = prefixSums[i + 1] - prefixSums[i + 1 - k];
-            } else {
-                left[i] = left[i - 1];
+
+        // Fill left array (max sum subarray on the left side)
+        int maxIndex = 0;
+        for (int i = 0; i < windowSums.size(); i++) {
+            if (windowSums[i] > windowSums[maxIndex]) {
+                maxIndex = i;
             }
+            left[i] = maxIndex;
         }
-        
-        // Compute best right indices
-        maxSum = prefixSums[n] - prefixSums[n - k];
-        for (int i = n - k - 1; i >= 0; --i) {
-            if (prefixSums[i + k] - prefixSums[i] >= maxSum) {
-                right[i] = i;
-                maxSum = prefixSums[i + k] - prefixSums[i];
-            } else {
-                right[i] = right[i + 1];
+
+        // Fill right array (max sum subarray on the right side)
+        maxIndex = windowSums.size() - 1;
+        for (int i = windowSums.size() - 1; i >= 0; i--) {
+            if (windowSums[i] >= windowSums[maxIndex]) { // Ensure lexicographical order
+                maxIndex = i;
             }
+            right[i] = maxIndex;
         }
-        
-        // Find the best three subarrays
-        vector<int> result(3, -1);
-        maxSum = 0;
-        for (int i = k; i <= n - 2 * k; ++i) {
-            int l = left[i - 1];
-            int r = right[i + k];
-            int totalSum = (prefixSums[l + k] - prefixSums[l]) + 
-                           (prefixSums[i + k] - prefixSums[i]) + 
-                           (prefixSums[r + k] - prefixSums[r]);
+
+        // Iterate for middle interval
+        int maxSum = 0;
+        for (int mid = k; mid < windowSums.size() - k; mid++) {
+            int leftIndex = left[mid - k];
+            int rightIndex = right[mid + k];
+            int totalSum = windowSums[leftIndex] + windowSums[mid] + windowSums[rightIndex];
             if (totalSum > maxSum) {
                 maxSum = totalSum;
-                result = {l, i, r};
+                result = {leftIndex, mid, rightIndex};
             }
         }
-        
+
         return result;
     }
 };
