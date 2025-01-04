2cac0048265d25569bb546ed79082f98c7df31d6
deepikaoppangi <126076250+deepikaoppangi@users.noreply.github.com>
2025-01-04 11:10:45 +0530

    Time: 816 ms (22.08%), Space: 11.6 MB (72.93%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -17,3 +17,41 @@ public:
         return fun(n-1,0,nums,target);
     }
 };  
+
+
+// class Solution {
+// public:
+//     int findTargetSumWays(vector<int>& nums, int target) {
+//         int n  = nums.size();
+//         int sum=0;
+//         for(int i=0;i<n;i++){
+//             sum=sum+nums[i];
+//         }
+//         int sum2= (sum-target)/2;
+//         vector<vector<int>> dp(n,vector<int>(sum2+1,0));
+
+//         if((sum-target)<0 || (sum-target)%2!=0){
+//             return 0;
+//         }
+//         if(nums[0]==0){
+//             dp[0][0]=2;
+//         }
+//         else if(nums[0]!=0){
+//             dp[0][0]=1;
+//         }
+//         if(nums[0]!=0 && nums[0]<=sum2){
+//             dp[0][nums[0]]=1;
+//         }
+//         for(int i=1;i<n;i++){
+//             for(int j=0;j<=sum2;j++){
+//                 int nonpick = dp[i-1][j];
+//                 int pick = 0;
+//                 if(j>=nums[i]){
+//                     pick = dp[i-1][j-nums[i]];
+//                 }
+//                 dp[i][j]=pick+nonpick;
+//             }
+//         }
+//         return dp[n-1][sum2];
+//     }
+// };  
