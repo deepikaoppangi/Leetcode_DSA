315134ceedfe96ca32077545bdde548229010735
deepikaoppangi <126076250+deepikaoppangi@users.noreply.github.com>
2025-01-06 12:32:19 +0530

    Time: 6 ms (17.52%), Space: 19.7 MB (17.13%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -35,14 +35,12 @@ public:
         dp[n][0]=dp[n][1] =0; 
         for(int i=n-1;i>=0;i--){
             for(int buy=0;buy<=1;buy++){
-                int profit = 0;
                 if(buy==0){
-                    profit = max(-prices[i]+dp[i+1][1] , dp[i+1][0]);
+                    dp[i][buy] = max(-prices[i]+dp[i+1][1] , dp[i+1][0]);
                 }
                 else{
-                    profit=max(prices[i]+dp[i+1][0] , dp[i+1][1]);
+                    dp[i][buy]=max(prices[i]+dp[i+1][0] , dp[i+1][1]);
                 }
-                dp[i][buy]=profit;
             }
         }
         return dp[0][0];
