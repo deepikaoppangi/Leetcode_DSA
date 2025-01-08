4a88be869aba64779ee66fdb4b7bc865aeb3d17b
deepikaoppangi <126076250+deepikaoppangi@users.noreply.github.com>
2025-01-08 19:07:40 +0530

    Time: 405 ms (27.15%), Space: 88.4 MB (31.7%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.cpp
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -72,22 +72,24 @@ public:
     int subseqlen(string s,string t){
         int m = s.size();
         int n = t.size();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
-        dp[0][0]=0;    //not required
-        for(int i=1;i<=m;i++){
-            dp[i][0]=0;      //not required
-        }
+        vector<int> prev(n+1,0);
+        vector<int> curr(n+1,0);
+        // dp[0][0]=0;    //not required
+        // for(int i=1;i<=m;i++){
+        //     dp[i][0]=0;      //not required
+        // }
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
                 if(s[i-1]==t[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
+                    curr[j]=1+prev[j-1];
                 }
                 else{
-                    dp[i][j]=dp[i][j-1];
+                    curr[j]=curr[j-1];
                 }
             }
+            prev=curr;
         }
-        return dp[m][n];
+        return prev[n];
     }
     int longestStrChain(vector<string>& words) {
         int n = words.size();
