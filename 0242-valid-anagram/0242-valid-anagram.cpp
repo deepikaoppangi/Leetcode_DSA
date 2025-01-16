3ef81dfa955f69c320f6f3991edf299e9e301ffd
deepikaoppangi <126076250+deepikaoppangi@users.noreply.github.com>
2025-01-16 14:04:06 +0530

    Time: 3 ms (57.65%), Space: 9.6 MB (69.47%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,24 +1,37 @@
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if(s.size()!=t.size()){
+//             return false;
+//         }
+//         map<char,int>mpp;
+//         int cnt=0;
+//         for(int i=0;i<s.size();i++){
+//             mpp[s[i]]++;
+//             cnt++;
+//         }
+//         for(int i=0;i<t.size();i++){
+//             if(mpp[t[i]]!=0){
+//                 mpp[t[i]]--;
+//                 cnt--;
+//             }    
+//         }
+//         if(cnt==0){
+//             return true;
+//         }
+//         return false;
+//     }
+// };
+
+
 class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size()){
             return false;
         }
-        map<char,int>mpp;
-        int cnt=0;
-        for(int i=0;i<s.size();i++){
-            mpp[s[i]]++;
-            cnt++;
-        }
-        for(int i=0;i<t.size();i++){
-            if(mpp[t[i]]!=0){
-                mpp[t[i]]--;
-                cnt--;
-            }    
-        }
-        if(cnt==0){
-            return true;
-        }
-        return false;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return (s==t);
     }
 };
