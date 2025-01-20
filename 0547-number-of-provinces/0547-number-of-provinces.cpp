f6b1cf8849c2ef55bff412b4deaf287d9f3dee04
deepikaoppangi <126076250+deepikaoppangi@users.noreply.github.com>
2025-01-20 10:17:56 +0530

    Time: 0 ms (100%), Space: 19.4 MB (71.73%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,25 +1,4 @@
-// class Solution {
-// public:
-//     void traversal(int node , vector<vector<int>>& isConnected ,vector<int> &visited_array ){
-//         visited_array[node]=1;
-//         for(int i = 0;i<isConnected[node].size();i++){
-//             if(isConnected[node][i]==1 && visited_array[i]==0)
-//             traversal(i,isConnected,visited_array);
-//         }
-//     }
-//     int findCircleNum(vector<vector<int>>& isConnected) {
-//         vector<int> visited_array(isConnected.size(),0);
-//         int count=0;
-//         for(int i = 0 ;i<isConnected.size();i++){
-//             if(visited_array[i]==0){
-//                 count++;
-//                 traversal(i,isConnected,visited_array);
-//             }
-//         }
-//         return count;
-//     }
-    
-// };
+
 
 class Solution {
 public:
