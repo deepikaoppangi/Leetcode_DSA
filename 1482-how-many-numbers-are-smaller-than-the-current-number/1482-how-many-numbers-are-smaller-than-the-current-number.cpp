ac91f774cb3aa135a29c250973f7fa51d2826d28
deepikaoppangi <126076250+deepikaoppangi@users.noreply.github.com>
2025-03-18 20:57:50 +0530

    Time: 3 ms (66.45%), Space: 14.7 MB (15.48%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,14 +1,35 @@
+// class Solution {
+// public:
+//     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int>res(n,0);
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 if(i!=j && nums[i]>nums[j]){
+//                     res[i]++;
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
         int n = nums.size();
         vector<int>res(n,0);
+        vector<int>sortednums=nums;
+        unordered_map<int,int>mpp;
+        sort(sortednums.begin(),sortednums.end());
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i!=j && nums[i]>nums[j]){
-                    res[i]++;
-                }
+            if(mpp.find(sortednums[i])==mpp.end()){
+                mpp[sortednums[i]]=i;
             }
+        }
+        for(int i=0;i<n;i++){
+            res[i]=mpp[nums[i]];
         }
         return res;
     }
