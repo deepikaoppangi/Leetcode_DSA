5e134ec799d1cc4999613795a0439dc0a5ad9ad2
deepikaoppangi <126076250+deepikaoppangi@users.noreply.github.com>
2025-03-18 13:22:57 +0530

    Time: 27 ms (75.46%), Space: 73.6 MB (50.65%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,11 +1,28 @@
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         unordered_map<int,int>mpp;
+//         for(int i=0;i<nums.size();i++){
+//             mpp[nums[i]]++;
+//             if(mpp[nums[i]]>1){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int>mpp;
+        unordered_set<int>sett;
         for(int i=0;i<nums.size();i++){
-            mpp[nums[i]]++;
-            if(mpp[nums[i]]>1){
+            if(sett.find(nums[i])!=sett.end()){
                 return true;
+            }
+            else{
+                sett.insert(nums[i]);
             }
         }
         return false;
