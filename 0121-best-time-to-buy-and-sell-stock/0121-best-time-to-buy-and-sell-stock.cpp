dd5b3fc4690c1631676379b9130afc76dd43c0e3
deepikaoppangi <126076250+deepikaoppangi@users.noreply.github.com>
2025-03-22 14:52:09 +0530

    Time: 0 ms (100%), Space: 97.2 MB (99.05%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -23,8 +23,8 @@ public:
         int mini = prices[0];
         int profit=0;
         for(int i=1;i<n;i++){
-            profit = max(profit,prices[i]-mini);
             mini = min(mini,prices[i]);
+            profit = max(profit,prices[i]-mini);
         }
         return profit;
     }
